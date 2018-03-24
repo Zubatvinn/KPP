326c89e425239c88c8f0ac5a1938b6325373a737
Ivan Khrishtapovich <zubat.vinn@gmail.com>
2018-03-24 21:04:37 +0200

    lab1
    First lab of KPP

---
+++ HelloJava1/src/tef/tm42/khrishpatovich/Model.java
@@ -0,0 +1,14 @@
+package tef.tm42.khrishpatovich;
+
+public class Model {
+
+    private String text;
+
+    public String getValue() {
+        return text;
+    }
+
+    public void setValue(String text) {
+        this.text = text;
+    }
+}
